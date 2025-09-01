0c9515c1504ebbca4eedd78ee6e4c176d5d34694
Édipo Marques <edipomarquesl@gmail.com>
2025-09-01 20:48:35 +0200

    EX003

--- Python/Ex003.py
+++ Python/Ex003.py
@@ -4,4 +4,4 @@ n1 = int(input('Type here the first number: '))
 n2 = int(input('Type here the second number: '))
 s = n1 + n2 
 
-print(f"The sum bewteen {n1} and {n2} is {s}")
+print(f"The sum of {n1} and {n2} is {s}")
