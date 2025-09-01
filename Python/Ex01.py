6f4a45cc5508d77582ba9cfd89a01edc38a77711
Édipo Marques <edipomarquesl@gmail.com>
2025-09-01 17:24:20 +0200

    Update Ex01.py

--- Python/Ex01.py
+++ Python/Ex01.py
@@ -1,5 +1,5 @@
 # Python Exercise 001 – Hello, World! 
 
-print("====================")
-print("Hello word, folks :)")
-print("====================")
+print("=============")
+print("Hello, World!")
+print("=============")
