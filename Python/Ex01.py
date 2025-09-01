6af584bd5f602e299465a8c2beda976ecb71d501
Édipo Marques <edipomarquesl@gmail.com>
2025-09-01 17:21:00 +0200

    update

--- Python/Ex01.py
+++ Python/Ex01.py
@@ -1,4 +1,6 @@
+# Python Exercise 001 – Hello, World! 
 
-
+print("====================")
 print("Hello word, folks :)")
+print("====================")
 print("Hope you all have a lovely journey ahead!")
