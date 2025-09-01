d873a76be0c9322d87f99a36e0ce9a2dfe39aa4e
Édipo Marques <edipomarquesl@gmail.com>
2025-09-01 21:03:09 +0200

    Ex004

---
+++ Python/Ex004.py
@@ -0,0 +1,10 @@
+"""
+
+------------- Python Exercise #004 -------------------
+Reading User Input and Showing Its Type and Properties.
+
+"""
+
+n = int(input("Type here a number: "))
+print(type(n))
+ 
