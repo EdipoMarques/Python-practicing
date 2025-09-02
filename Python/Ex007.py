b800d77dd7661351a17c9d4abd44ba154ac1983e
Édipo Marques <edipomarquesl@gmail.com>
2025-09-02 13:41:09 +0200

    Ex007

---
+++ Python/Ex007.py
@@ -0,0 +1,11 @@
+"""
+            Python Exercise #007 
+Reading Two Grades and Calculating the Average
+
+"""
+
+n1 = float(input("Type here your first grade: "))
+n2 = float(input("Type here your second grade: "))
+t = (n1+n2)/2
+
+print(f"Your final grade is {t}")
