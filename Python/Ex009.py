33255cd2d39d8b1b8bdf98221f12c37ff3c9b41d
Édipo Marques <edipomarquesl@gmail.com>
2025-09-03 00:19:48 +0200

    Ex009

---
+++ Python/Ex009.py
@@ -0,0 +1,20 @@
+"""
+                Python Exercise #009  
+Reading an Integer and Displaying Its Multiplication Table
+
+"""
+
+n = int(input("Type here a number: "))
+
+print("n x 0" == (n*0))
+print("n x 1" == (n*1))
+print("n x 2" == (n*2))
+print("n x 3" == (n*3))
+print("n x 4" == (n*4))
+print("n x 5" == (n*5))
+print("n x 6" == (n*6))
+print("n x 7" == (n*7))
+print("n x 8" == (n*8))
+print("n x 9" == (n*9))
+print("n x 10" == (n*10))
+
