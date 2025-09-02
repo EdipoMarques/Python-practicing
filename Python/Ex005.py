828ff3d5581ec4b2e8e33833cc8e813482426204
Édipo Marques <edipomarquesl@gmail.com>
2025-09-02 03:33:30 +0200

    Ex005

---
+++ Python/Ex005.py
@@ -0,0 +1,10 @@
+"""
+                Python Exercise #005 
+Reading an Integer and Showing Its Successor and Predecessor
+
+"""
+
+n1 = int(input("Type here a number: "))
+successor = n1 + 1
+predecessor = n1 - 1
+print(f"The predecessor of {n1} is {predecessor} and its sucessor is {successor}.")
