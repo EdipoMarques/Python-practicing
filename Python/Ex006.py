679abd19db7fda0db9dbff1b4686f6cdce666c5a
Édipo Marques <edipomarquesl@gmail.com>
2025-09-02 12:35:56 +0200

    Ex006

---
+++ Python/Ex006.py
@@ -0,0 +1,12 @@
+"""
+                    Python Exercise #006 
+Reading a Number and Showing Its Double, Triple, and Square Root
+
+"""
+
+n = int(input("Type here a number: "))
+double = n * 2 
+triple = n * 3
+sr = n * (1/2)
+
+print(f"The double of {n} is {double}, triple is {triple} and the square root is {sr}" )
