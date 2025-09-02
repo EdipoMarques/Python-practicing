af7e98862b838aca66b005a25f025f73024444b3
Édipo Marques <edipomarquesl@gmail.com>
2025-09-02 03:35:49 +0200

    Ex002

--- Python/Ex002.py
+++ Python/Ex002.py
@@ -1,4 +1,4 @@
 # Python Exercise #002 – Responding to the User
 
 name = input('Whats is your name? ')
-print(f"Hey {name}, welcome to the course :)")
+print(f"Hey {name}, welcome to the python's course :)")
