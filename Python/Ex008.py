8933edb0d32daf3d4e7cb6f038babf942fea380f
Édipo Marques <edipomarquesl@gmail.com>
2025-09-02 18:12:22 +0200

    Ex008

---
+++ Python/Ex008.py
@@ -0,0 +1,11 @@
+"""
+            Python Exercise #008 
+Converting Meters to Centimeters and Millimeters
+
+"""
+
+distance = float(input("Type here the distance in meters: "))
+centimenters = (distance * 100)
+millimetres = (distance * 1000)
+
+print(f"The distance in centimenters is {centimenters} and {millimetres} in millimetres")
