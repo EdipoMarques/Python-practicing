f3aee39d3d02a5f7f3a42e729c322cc0e5da2ab8
Édipo Marques <edipomarquesl@gmail.com>
2025-09-02 03:36:30 +0200

    Ex003

--- Python/Ex003.py
+++ Python/Ex003.py
@@ -1,7 +1,7 @@
 # Python Exercise #003 – Reading Two Numbers and Showing Their Sum 
 
-n1 = int(input('Type here the first number: '))
-n2 = int(input('Type here the second number: '))
+n1 = int(input('Type the first number: '))
+n2 = int(input('Type the second number: '))
 s = n1 + n2 
 
 print(f"The sum of {n1} and {n2} is {s}")
