642dbd48aef09e2ce88a9ab6a28434ffbff37f79
Édipo Marques <edipomarquesl@gmail.com>
2025-09-04 20:32:55 +0200

    Ex012.py

---
+++ Python/Ex012.py
@@ -0,0 +1,9 @@
+#012 – Price Calculator with 5% Discount
+#
+#This program calculates the new price of a product after applying a 5% discount 🛒.  
+#
+#The user provides the **original price** of the product.  
+#The program then applies a 5% discount and shows the **final discounted price**.  
+#
+#💡 Example:  
+#If the original price is $100.00 → the program will calculate 5% off and return $95.00.
