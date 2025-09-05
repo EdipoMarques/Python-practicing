4d0421e992b127a276c45cff27b4b3d2922fb84d
Édipo Marques <edipomarquesl@gmail.com>
2025-09-06 01:45:17 +0200

    Ex015

--- Python/Ex.015.py
+++ Python/Ex.015.py
@@ -6,3 +6,9 @@
 #- If the user inputs -4.89 → the program will output -4 as the integer part.  
 #
 #To achieve this, the program uses Python’s built-in functions or the math library to truncate the decimal part of the number.
+
+import math
+num = float(input("Enter here a number: "))
+print(f"The integer part of the number {num} is {math.trunc(num)}")
+#or
+print(f"The integer part of the number {num} is {int(num)}")
