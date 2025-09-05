6b4a026fc625e21861f0fcc115f0205e059a8e2f
Édipo Marques <edipomarquesl@gmail.com>
2025-09-06 01:42:07 +0200

    Ex015

--- Python/Ex.015.py
+++ Python/Ex.015.py
@@ -1,1 +1,8 @@
 #Python Exercise #016 – Extracting the Integer Part of a Real Number
+#This program reads a real number entered by the user and displays its integer part only 🔢.  
+
+#For example:  
+#- If the user inputs 6.127 → the program will output 6 as the integer part.  
+#- If the user inputs -4.89 → the program will output -4 as the integer part.  
+#
+#To achieve this, the program uses Python’s built-in functions or the math library to truncate the decimal part of the number.
