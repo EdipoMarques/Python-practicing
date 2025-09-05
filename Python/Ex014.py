84366d9bcdc1bbb9eeb9c1e71ddc4fc072968a02
Édipo Marques <edipomarquesl@gmail.com>
2025-09-05 07:23:49 +0200

    Ex014

--- Python/Ex014.py
+++ Python/Ex014.py
@@ -2,4 +2,10 @@
 #This program converts a temperature given in Celsius (°C) to Fahrenheit (°F) 🌡️.  
 #The user inputs a temperature in Celsius, and the program applies the conversion formula:  
 #   Fahrenheit = (Celsius × 9/5) + 32  
-#The result is then displayed as the equivalent temperature in Fahrenheit.  
+#The result is then displayed as the equivalent temperature in Fahrenheit.
+# 
+
+c = float(input("Enter here the temperature i ncelsius: C")) 
+f = (c * 9/5) + 32
+print(f"The temperature of {c}°C is equivalent to {f}°F")
+ 
