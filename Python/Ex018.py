7710b2408865abbfff5b230d6ac3efbc16d0ccdc
Édipo Marques <edipomarquesl@gmail.com>
2025-09-13 04:16:54 +0200

    Ex018

--- Python/Ex018.py
+++ Python/Ex018.py
@@ -1,9 +1,10 @@
 
+from math import radians, sin, cos, tan
 
 angle = float(input('Enter here the angle: '))
-seno = math.sin(math.radians(angle))
-cosseno = math.cos(math.radians(angle))
-tangente = math.tan(math.radians(angle))
+seno = sin(radians(angle))
+cosseno = cos(radians(angle))
+tangente = tan(radians(angle))
 print(f"O ângulo de {angle} tem o SENO de {seno}")
 print(f"O ângulo de {angle} tem o COSSENO de {cosseno} ")
 print(f"O ângulo de {angle} tem a TANGENTE de {tangente}")
